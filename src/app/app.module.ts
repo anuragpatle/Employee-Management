82622837ddd4dd6e57a05b372e112c2a76b712d8
anuragpatle <anurag.ap6@gmail.com>
2020-04-04 11:47:13 +0530

    Implemented Auth for roles user and admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+// used to create fake backend
+import { fakeBackendProvider } from '@app/_helpers';
+
+import { AppComponent } from '@app/app.component';
+import { appRoutingModule } from '@app/app-routing.module';
+
+import { JwtInterceptor, ErrorInterceptor } from '@app/_helpers';
+import { HomeComponent } from '@app/home';
+import { AdminComponent } from '@app/admin';
+import { LoginComponent } from '@app/login';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    appRoutingModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AdminComponent,
+    LoginComponent
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+    // provider used to create fake backend
+    fakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
